e9e01da8ad647190144e225150cfe056538d4742
Ibochkarev <ivanx86@gmail.com>
2021-05-23 19:11:59 +0600

    Добавлено мобильное меню, слайдер экспертов, лого

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,29 @@
-import $ from '../local_modules/jquery/dist/jquery.min'
+import { Swiper, Mousewheel, Navigation } from 'swiper'
+Swiper.use([Mousewheel, Navigation])
 
-$(document).ready(() => {
-    // eslint-disable-next-line no-console
-    console.log(`document ready`)
+document.addEventListener(`DOMContentLoaded`, () => {
+    const expertsSlider = new Swiper(`.expert-slider`, {
+        loop: false,
+        speed: 800,
+        slidesPerView: 3,
+        slidesPerColumn: 2,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.slideNext-btn',
+            prevEl: '.slidePrev-btn',
+        },
+    })
+
+    const mobileMenu = document.querySelector(`.mobile-menu`),
+        menuBtn = document.querySelector(`.header__btn-menu`)
+
+    menuBtn.addEventListener(`click`, function () {
+        if (mobileMenu.classList.contains(`is-active`)) {
+            mobileMenu.classList.remove(`is-active`)
+            menuBtn.classList.remove(`is-open`)
+        } else {
+            mobileMenu.classList.add(`is-active`)
+            menuBtn.classList.add(`is-open`)
+        }
+    })
 })
